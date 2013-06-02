f978e64b630ce8ca9df69517f9e49c664e40ec82
Per Lundberg <per@halleluja.nu>
2013-06-02 23:12:03 +0300

    Use #pragma once instead of old-school #ifnfdef/endif stuff.

--- storm/include/storm/generic/arguments.h
+++ storm/include/storm/generic/arguments.h
@@ -1,23 +1,19 @@
-/* Abstract: Function prototypes for command line argument parsing
-   routines. */
+// Abstract: Function prototypes for command line argument parsing routines.
 
-/* Copyright 1999-2000, 2013 chaos development. */
+// © Copyright 1999-2000, 2013 chaos development.
 
-#ifndef __STORM_I386_ARGUMENTS_H__
-#define __STORM_I386_ARGUMENTS_H__
+#pragma once
 
 #include <storm/types.h>
-
-/* Function prototypes. */
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
-  extern u32 arguments_parse (char *source, char *destination, u32 delta);
-  extern char arguments_kernel[];
+
+extern u32 arguments_parse (char *source, char *destination, u32 delta);
+extern char arguments_kernel[];
+
 #ifdef __cplusplus
 }
 #endif
-
-#endif /* !__STORM_I386_ARGUMENTS_H__ */
