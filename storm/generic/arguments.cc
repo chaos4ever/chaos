0a9f008a6edc51d03cc934d81a949008d5600c01
Per Lundberg <per@halleluja.nu>
2013-06-02 10:01:54 +0300

    Let's try to uncrustify the files a bit. I think the chaos coding standards are a bit outdated; we should have a little bit more space in some places and less elsewhere. :) I also added some more usage of the auto keyword.

--- storm/generic/arguments.cc
+++ storm/generic/arguments.cc
@@ -16,77 +16,76 @@ static int find_number_of_arguments (char *source);
    more spaces). */
 
 extern "C"
-u32 arguments_parse (char *source, char *destination, u32 delta)
+u32 arguments_parse(char *source, char *destination, u32 delta)
 {
-  int args = find_number_of_arguments(source);
+    auto args = find_number_of_arguments(source);
 
-  /* Save this value. */
-  
-  *((u32 *) destination) = args;
+    /* Save this value. */
 
-  /* Now, go through the string again and copy the parameters into the
-     data area and set the pointers correctly. */
-  /* FIXME: Avoid pointer arithmetic. We should have a pure structure
-     for this. I'm not sure if it is possible to write this in a clean
-     and nice way at all. */
+    *((u32 *) destination) = args;
 
-  auto word_pointer = (char **) (destination + 4);
-  auto word = (char *) (destination + 4 + sizeof (char *) * args);
+    /* Now, go through the string again and copy the parameters into the
+       data area and set the pointers correctly. */
+    /* FIXME: Avoid pointer arithmetic. We should have a pure structure
+       for this. I'm not sure if it is possible to write this in a clean
+       and nice way at all. */
 
-  /* OK, the initial pointers are setup. */
+    auto word_pointer = (char **) (destination + 4);
+    auto word = (char *) (destination + 4 + sizeof (char *) * args);
 
-  for (int position = 0, arg = 0; source[position] != '\0'; position++)
-  {
-    while (source[position] == ' ')
+    /* OK, the initial pointers are setup. */
+
+    for (auto position = 0, current_arg = 0; source[position] != '\0'; position++)
     {
-      position++;
+        while (source[position] == ' ')
+        {
+            position++;
+        }
+
+        if (source[position] != '\0')
+        {
+            auto word_length = 0;
+            while (source[position + word_length] != ' ' &&
+                   source[position + word_length] != '\0')
+            {
+                word_length++;
+            }
+
+            string_copy_max(word, &source[position], word_length);
+
+            position += word_length - 1;
+            word[word_length] = 0;
+            word_pointer[current_arg] = word + delta;
+            word += word_length + 1;
+
+            current_arg++;
+        }
+        else break;
     }
 
-    if (source[position] != '\0')
-    {
-      /* Find out the length of the word. */
-
-      int length = 0;
-      while (source[position + length] != ' ' &&
-             source[position + length] != '\0')
-      {
-        length++;
-      }
-
-      string_copy_max (word, &source[position], length);
-
-      position += length - 1;
-      word[length] = 0;
-      word_pointer[arg] = word + delta;
-      word += length + 1;
-      arg++;
-    }
-    else break;
-  }
-
-  return args;
+    return args;
 }
 
-static int find_number_of_arguments (char *source)
+static int find_number_of_arguments(char *source)
 {
-  int position = 0, result = 0;
+    int position = 0, result = 0;
 
-  while (source[position] != '\0')
-  {
-    if (source[position] != ' ')
+    while (source[position] != '\0')
     {
-      result++;
+        if (source[position] != ' ')
+        {
+            result++;
 
-      while (source[position] != ' ' && source[position] != '\0')
-      {
-        position++;
-      }
+            while (source[position] != ' ' && source[position] != '\0')
+            {
+                position++;
+            }
+        }
+        else
+        {
+            position++;
+        }
     }
-    else
-    {
-      position++;
-    }
-  }
 
-  return result;
+    return result;
 }
