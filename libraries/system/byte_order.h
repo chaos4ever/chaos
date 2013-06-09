cf2e2e2569323957469e6de4bcd952f9a553364f
Per Lundberg <per@halleluja.nu>
2013-06-09 11:20:06 +0300

    Fixed the byte_order header so that it now compiles (and dropped 386 support, since it's not really needed any more). The string library therefore compiles successfully - wheegit add ../system/byte_order.h

--- libraries/system/byte_order.h
+++ libraries/system/byte_order.h
@@ -1,67 +1,32 @@
-/* $Id$ */
-/* Abstract: Inline functions for swapping byte order. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: Inline functions for swapping byte order.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// Copyright 1999-2000, 2007, 2013 chaos development.
 
-/* Copyright 1999-2000 chaos development. */
-/* Copyright 2007 chaos development. */
-
-#ifndef __LIBRARY_SYSTEM_BYTE_ORDER_H__
-#define __LIBRARY_SYSTEM_BYTE_ORDER_H__
+#pragma once
 
 #include <storm/storm.h>
 
-/* 486 and above have the neat instruction 'bswap' which makes life a
-   lot easier. */
-/* FIXME: This doesn't work for some reason. Find out why! (It always
-   includes this code) */
-
-#if (TARGET_CPU == i486) || (TARGET_CPU == i586) || (TARGET_CPU == i686) || (TARGET_CPU == k6)
-
-static inline u32 system_byte_swap_u32 (u32 x)
+// 486 and above have the neat instruction 'bswap' which makes life a lot easier. Since 99,9% of all Intel machines that you
+// reasonably want to run chaos on is 486 or greater, there's little point in supporting 386 at this point.
+static inline u32 system_byte_swap_u32(u32 x)
 {
-  /* Swap the bytes. */
 
-  asm 
-  ("bswap  %0"
-   : "=r" (x)
-   : "0" (x));
-  
-  return x;
+    // Swap the bytes.
+    asm
+    ("bswap  %0"
+     : "=r" (x)
+     : "0" (x));
+
+    return x;
 }
 
-#else
-
-static inline const u32 system_byte_swap_u32 (u32 x)
+static inline u16 system_byte_swap_u16(u16 x)
 {
-  asm 
-  ("\
-    /* Swap lower bytes. */
+    asm
+    ("xchgb   %b0, %h0"
+     : "=q" (x)
+     : "0" (x));
 
-    xchgb       %b0, %h0
-
-    /* Swap u16s. */
-
-    rorl        $16, %0
-
-    /* Swap higher bytes. */
-
-    xchgb       %b0, %h0
-   "
-   : "=q" (x)
-   : "0" (x));
-
-  return x;
+    return x;
 }
-
-#endif /* (!defined __i486__) && (!defined __i586__) && (!defined __i686) */
-
-static inline u16 system_byte_swap_u16 (u16 x)
-{
-  asm 
-  ("xchgb   %b0, %h0"
-   : "=q" (x) 
-   : "0" (x));
-  return x;
-}
-
-#endif /* !__LIBRARY_SYSTEM_BYTE_ORDER_H__ */
