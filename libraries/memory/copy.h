887b68fcd45cf7584dff35dc5f00747a0cac43a2
Per Lundberg <per@halleluja.nu>
2013-06-11 20:23:22 +0300

    Fixed to get rid of a compilation warning (cannot assign to different type without cast, -fpermissive).

--- libraries/memory/copy.h
+++ libraries/memory/copy.h
@@ -8,10 +8,10 @@
 extern inline void *memory_copy(void *to, const void *from, int n);
 
 // TODO: Add architecture-optimized versions of this, probably as header files in storm by then.
- void *memory_copy(void * restrict to, const void * restrict from, int n)
+ void *memory_copy(void * __restrict__ to, const void * __restrict__ from, int n)
  {
-   char *dst = to;
-   const char *src = from;
+   char *dst = (char *) to;
+   const char *src = (const char *) from;
 
    while (n-- != 0)
    {
