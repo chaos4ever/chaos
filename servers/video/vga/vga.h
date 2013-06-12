0c16674e9788ac80a63b4e0cc3b833e34d4ca487
Per Lundberg <per@halleluja.nu>
2013-06-12 23:59:49 +0300

    Again, #2.

--- servers/video/vga/vga.h
+++ servers/video/vga/vga.h
@@ -1,26 +1,9 @@
-/* $Id$ */
-/* Abstract: VGA header file. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: VGA header file.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000, 2013 chaos development.
 
-/* Copyright 2000 chaos development. */
-
-/* This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __VGA_H__
-#define __VGA_H__
+#pragma once
 
 #define VGA_PORT_BASE           0x3C0
 #define VGA_PORTS               32
@@ -34,5 +17,3 @@
 
 #define VGA_MEMORY              0xA0000
 #define VGA_MEMORY_SIZE         (64 * KB)
-
-#endif /* !__VGA_H__ */
