9d48e8cc9614db57b71e4110f2eea8b23581d54c
Per Lundberg <per@halleluja.nu>
2013-06-12 23:34:22 +0300

    More work on #2.

--- libraries/system/system.h
+++ libraries/system/system.h
@@ -1,26 +1,9 @@
-/* $Id$ */
-/* Abstract: System library main header file. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: System library main header file.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000, 2013 chaos development.
 
-/* Copyright 2000 chaos development. */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __LIBRARY_SYSTEM_SYSTEM_H__
-#define __LIBRARY_SYSTEM_SYSTEM_H__
+#pragma once
 
 #include <system/alignment.h>
 #include <system/bit.h>
@@ -30,5 +13,3 @@
 #include <system/port.h>
 #include <system/return_values.h>
 #include <system/system_calls.h>
-
-#endif /* !__LIBRARY_SYSTEM_SYSTEM_H__ */
