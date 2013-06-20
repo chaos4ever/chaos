e67d6369cb9b199a48b7d767adbc53e629ca2bc7
Per Lundberg <per@halleluja.nu>
2013-06-20 23:53:31 +0300

    Style fixes a la #2. Also changed mutex_signal to actually do something a bit more useful... ;) mutex_wait is still unimplemented though.

--- libraries/mutex/mutex.h
+++ libraries/mutex/mutex.h
@@ -1,61 +1,39 @@
-/* $Id$ */
-/* Abstract: Mutex library function prototypes. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: Mutex library function prototypes.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000, 2013 chaos development.
 
-/* Copyright 2000 chaos development. */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __LIBRARY_MUTEX_MUTEX_H__
-#define __LIBRARY_MUTEX_MUTEX_H__
+#pragma once
 
 enum
 {
-  MUTEX_UNLOCKED,
-  MUTEX_LOCKED
+    MUTEX_UNLOCKED,
+    MUTEX_LOCKED
 };
 
 typedef volatile unsigned int mutex_type;
 
-/* Lock a mutex. */
-
-static inline void mutex_wait (mutex_type mutex __attribute__ ((unused)))
+// Lock a mutex.
+static inline void mutex_wait(mutex_type mutex UNUSED)
 {
-  /* FIXME: Fix this code. */
+    // FIXME: Fix this code. For now, user-level mutexing is broken. We should perhaps 
 
 #if FALSE
-  unsigned int eax;
+    unsigned int eax;
 
-  asm volatile 
-  ("\
-    movl $0, %%eax
-0:  xchgl %%eax, %0
-    cmpl $0, %%eax
-    je 0b
-   "
-   : "=g" (mutex), "=a" (eax)
-   : "bcdDS" (mutex));         /* BCD is faster than integers -- Martim. */
+    asm volatile("movl $0, %%eax\n"
+                 "0:  xchgl %%eax, %0\n"
+                 "cmpl $0, %%eax\n"
+                 "je 0b"
+                 : "=g" (mutex),
+                   "=a" (eax)
+                 : "bcdDS" (mutex));    // BCD is faster than integers -- Martim.
+
 #endif
 }
 
-/* Unlock a mutex. */
-
-static inline void mutex_signal (mutex_type mutex)
+// Unlock a mutex.
+static inline void mutex_signal(mutex_type *mutex)
 {
-  mutex = MUTEX_UNLOCKED;
+    *mutex = MUTEX_UNLOCKED;
 }
-
-#endif /* !__LIBRARY_MUTEX_MUTEX_H__ */
