e40ceb2f59b2a23ed4620696fc3daa821139f558
Per Lundberg <per@halleluja.nu>
2013-06-20 23:59:27 +0300

    Fixed up a la #2.

--- libraries/memory/memory.h
+++ libraries/memory/memory.h
@@ -1,28 +1,11 @@
-/* $Id$ */
-/* Abstract: Memory functions. */
-/* Author: Anders Ohrt <doa@chaosdev.org>
-            Henrik Hallin <hal@chaosdev.org>
-             Per Lundberg <sug-per@halleluja.nu> */
+// Abstract: Memory functions.
+// Author: Anders Ohrt <doa@chaosdev.org>
+//         Henrik Hallin <hal@chaosdev.org>
+//         Per Lundberg <sug-per@halleluja.nu>
+//
+// © Copyright 2000, 2013 chaos development.
 
-/* Copyright 2000 chaos development. */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __LIBRARY_MEMORY_MEMORY_H__
-#define __LIBRARY_MEMORY_MEMORY_H__
+#pragma once
 
 #include <memory/copy.h>
 #include <memory/functions.h>
@@ -30,5 +13,3 @@
 #include <memory/set.h>
 #include <memory/types.h>
 #include <memory/types.h>
-
-#endif /* !__LIBRARY_MEMORY_MEMORY_H__ */
