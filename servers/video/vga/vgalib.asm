5928cb19dcb006aa4380f542c9e4ce66af7a5c44
Per Lundberg <per@halleluja.nu>
2013-06-21 22:09:15 +0300

    Cleanups a la #2.

--- servers/video/vga/vgalib.asm
+++ servers/video/vga/vgalib.asm
@@ -1,26 +1,9 @@
-; $Id: vgalib.asm,v 1.2 2000/10/22 21:38:16 plundis Exp $
-; Abstract: VGA library in assembly
+; Abstract: x86 assembly-level VGA helper functions
+; Author: Johan Thim (Thank you for your great help!)
+;
+; Copyright 1999-2000, 2013 chaos development.
 
-; Copyright 1999-2000 chaos development.
-
-; Originally written by Johan Thim. Thank you for your great help!
-
-; This program is free software; you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation; either version 2 of the License, or
-; (at your option) any later version.
-
-; This program is distributed in the hope that it will be useful, but
-; WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-; General Public License for more details.
-
-; You should have received a copy of the GNU General Public License
-; along with this program; if not, write to the Free Software
-; Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-; USA.
-
-; FIXME: Rewrite this in C!!!
+; FIXME: Rewrite this in C!
 
 extern          font_8x8
 extern          graphic_video_memory
@@ -74,12 +57,6 @@ vga_buff        dd      miff
 miff		times 300 db 0
 
 section		.text
-
-; IN:
-; bx = video mode
-;
-; OUT:
-; eax = 0 if success, else eax = 0xFFFFFFFF
 
 vga_set_mode:   push    ebp
 		mov	ebp, esp
