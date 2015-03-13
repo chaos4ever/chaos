42559e5b0a224f1559d8ac2cce532ea32bd38f79
Per Lundberg <perlun@gmail.com>
2015-03-13 23:33:23 +0200

    Fixed the copyright header.

--- storm/ia32/process.c
+++ storm/ia32/process.c
@@ -2,7 +2,9 @@
 // Authors: Henrik Hallin <hal@chaosdev.org>
 //          Per Lundberg <per@halleluja.nu>
 //
-// © Copyright 1999-2000, 2007, 2013, 2015 chaos development.
+// © Copyright 1999-2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2013-2015 chaos development
 
 // FIXME: Most of this should be put into the generic subdirectory.
 
