9434e33c98898124426a0d3ae56b008178d0031b
Per Lundberg <perlun@gmail.com>
2015-04-20 20:31:15 +0300

    Updated copyrights.

--- storm/ia32/memory_global.c
+++ storm/ia32/memory_global.c
@@ -2,7 +2,9 @@
 // Authors: Henrik Hallin <hal@chaosdev.org>
 //          Per Lundberg <per@halleluja.nu>
 //
-// © Copyright 2000, 2013, 2015 chaos development.
+// © Copyright 2000 chaos development
+// © Copyright 2013 chaos development
+// © Copyright 2015 chaos development
 
 // If DEBUG is TRUE, lots of debug information will be printed.
 
