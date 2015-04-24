17a392fab25e9a3ff9b3218f6cb65fb2a99a80c4
Per Lundberg <perlun@gmail.com>
2015-04-24 21:16:07 +0300

    We need to set the return value in line with whether the call failed or not.

--- tools/colorize.rb
+++ tools/colorize.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby
 
+$return_value = 0
+
 def colorize(s, color_code)
   "\e[#{color_code}m#{s}\e[0m"
 end
@@ -15,8 +17,10 @@ end
 while gets
   $_.gsub!(/ OK /) { |s| green s }
   $_.gsub!(/  PASSED/) { |s| green s }
-  $_.gsub!(/  FAILED/) { |s| red s }
-  $_.gsub!(/\d+ FAILED.*/) { |s| red s }
+  $_.gsub!(/  FAILED/) { |s| $return_value = 1; red s; }
+  $_.gsub!(/\d+ FAILED.*/) { |s| $return_value = 1; red s; }
 
   puts $_
 end
+
+exit $return_value
