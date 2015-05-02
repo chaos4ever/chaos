751bf0aa0b09c675f7d6ee66761e3558f9966d32
Per Lundberg <perlun@gmail.com>
2015-05-02 22:51:07 +0300

    Added a ouple of string tests as well.

--- storm_tests/ia32/ia32_tests_main.c
+++ storm_tests/ia32/ia32_tests_main.c
@@ -7,14 +7,25 @@
 
 extern void test_memory_global_allocate(void **state);
 extern void test_memory_global_deallocate(void **state);
+extern void string_to_number_decimal(void **state);
+extern void string_to_number_negative_decimal(void **state);
+extern void string_to_number_invalid_value(void **state);
 
 int main(void)
 {
-    const struct CMUnitTest tests[] =
+    const struct CMUnitTest memory_global_tests[] =
     {
         cmocka_unit_test(test_memory_global_allocate),
         cmocka_unit_test(test_memory_global_deallocate),
     };
-    return cmocka_run_group_tests(tests, NULL, NULL);
-}
 
+    const struct CMUnitTest string_tests[] =
+    {
+        cmocka_unit_test(string_to_number_decimal),
+        cmocka_unit_test(string_to_number_negative_decimal),
+        cmocka_unit_test(string_to_number_invalid_value)
+    };
+    
+    cmocka_run_group_tests_name("memory_global", memory_global_tests, NULL, NULL);
+    cmocka_run_group_tests_name("string", string_tests, NULL, NULL);
+}
