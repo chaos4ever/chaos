90eea90ecdfa5d9332254e1f903ad0e83bc8f6a3
Per Lundberg <perlun@gmail.com>
2015-05-02 22:59:39 +0300

    Added more string unit tests.

--- storm_tests/ia32/ia32_tests_main.c
+++ storm_tests/ia32/ia32_tests_main.c
@@ -10,6 +10,8 @@ extern void test_memory_global_deallocate(void **state);
 extern void string_to_number_decimal(void **state);
 extern void string_to_number_negative_decimal(void **state);
 extern void string_to_number_invalid_value(void **state);
+extern void string_to_number_hexadecimal(void **state);
+extern void string_to_number_binary(void **state);
 
 int main(void)
 {
@@ -23,7 +25,9 @@ int main(void)
     {
         cmocka_unit_test(string_to_number_decimal),
         cmocka_unit_test(string_to_number_negative_decimal),
-        cmocka_unit_test(string_to_number_invalid_value)
+        cmocka_unit_test(string_to_number_invalid_value),
+        cmocka_unit_test(string_to_number_hexadecimal),
+        cmocka_unit_test(string_to_number_binary)
     };
     
     cmocka_run_group_tests_name("memory_global", memory_global_tests, NULL, NULL);
---
+++ storm_tests/ia32/string_tests.c
@@ -0,0 +1,48 @@
+// Abstract: String routines (unit tests)
+// Authors: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2015 chaos development
+
+#include "test_helper.h"
+
+#include <storm/generic/string.h>
+
+void string_to_number_decimal(void **state)
+{
+    int i;
+    string_to_number("10", &i);
+
+    assert_int_equal(i, 10);
+}
+
+void string_to_number_negative_decimal(void **state)
+{
+    int i;
+    string_to_number("-42", &i);
+
+    assert_int_equal(i, -42);
+}
+
+void string_to_number_invalid_value(void **state)
+{
+    int i;
+    return_type result = string_to_number("foo", &i);
+
+    assert_int_equal(result, RETURN_INVALID_ARGUMENT);
+}
+
+void string_to_number_hexadecimal(void **state)
+{
+    int i;
+    string_to_number("0xBADBEEF", &i);
+
+    assert_int_equal(i, 0xBADBEEF);
+}
+
+void string_to_number_binary(void **state)
+{
+    int i;
+    string_to_number("0b00101010", &i);
+
+    assert_int_equal(i, 42);
+}
