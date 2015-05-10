d189820eb4bc5e94cca9459f7936cfc834a12ae6
Per Lundberg <perlun@gmail.com>
2015-05-10 15:42:17 +0300

    Changes in line with #2.

--- libraries/system/system_calls.h
+++ libraries/system/system_calls.h
@@ -2,7 +2,10 @@
 // Authors: Henrik Hallin <hal@chaosdev.org>,
 //          Per Lundberg <per@halleluja.nu>
 //
-// © Copyright 1999-2000, 2007, 2013 chaos development.
+// © Copyright 1999-2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2013 chaos development
+// © Copyright 2015 chaos development
 
 #pragma once
 
