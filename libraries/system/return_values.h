6e6c0e47c74b7239cc3bb0fc7aaf057bca1a2f1f
Per Lundberg <perlun@gmail.com>
2015-05-10 15:34:22 +0300

    Fixes in line with #2.

--- libraries/system/return_values.h
+++ libraries/system/return_values.h
@@ -1,26 +1,10 @@
-/* $Id$ */
-/* Abstract: Return values used by the system library. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: Return values used by the system library.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000 chaos development
+// © Copyright 2015 chaos development
 
-/* Copyright 2000 chaos development. */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __LIBRARY_SYSTEM_RETURN_VALUES_H__
-#define __LIBRARY_SYSTEM_RETURN_VALUES_H__
+#pragma once
 
 enum
 {
@@ -39,5 +23,3 @@ enum
 
   SYSTEM_RETURN_THREAD_CREATE_FAILED,
 };
-
-#endif /* !__LIBRARY_SYSTEM_RETURN_VALUES_H__ */
