5ae740bfaa5938573494bb3d8444559afbf1bebd
Per Lundberg <perlun@gmail.com>
2015-05-10 14:53:56 +0300

    Updated copyrights.

--- storm/include/storm/return_values.h
+++ storm/include/storm/return_values.h
@@ -3,7 +3,9 @@
 //          Henrik Hallin <hal@chaosdev.org>
 //          Anders Ohrt <doa@chaosdev.org>
 //
-// © Copyright chaos development 1999-2000, 2013.
+// © Copyright 1999-2000 chaos development
+// © Copyright 2013 chaos development
+// © Copyright 2015 chaos development
 
 #pragma once
 
