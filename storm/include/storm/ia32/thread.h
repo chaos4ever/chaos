a2b7d5284755bbd77f63342f7041ea5f275e183f
Per Lundberg <perlun@gmail.com>
2015-05-10 15:18:05 +0300

    Changes in line with #2.

--- storm/include/storm/ia32/thread.h
+++ storm/include/storm/ia32/thread.h
@@ -1,12 +1,13 @@
 // Abstract: IA32-specific thread stuff.
 // Author: Per Lundberg <per@halleluja.nu>
 //
-// © Copyright 2000, 2013 chaos development.
+// © Copyright 2000 chaos development
+// © Copyright 2013 chaos development
+// © Copyright 2015 chaos development
 
 #pragma once
 
 #include <storm/ia32/flags.h>
 
 // Default flags for new processes are IF, PF, AF. See the Intel documentation for more information about this.
-#define THREAD_NEW_EFLAGS (FLAG_INTERRUPT_ENABLE | FLAG_ADJUST | \
-                           FLAG_PARITY | FLAG_SET)
+#define THREAD_NEW_EFLAGS (FLAG_INTERRUPT_ENABLE | FLAG_ADJUST | FLAG_PARITY | FLAG_SET)
