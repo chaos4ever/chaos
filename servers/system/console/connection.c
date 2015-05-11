e7dd6ebc1d2d910b9f103b7154ed970c3cfcdd25
Per Lundberg <perlun@gmail.com>
2015-05-11 21:35:30 +0300

    Again, #2.

--- servers/system/console/connection.c
+++ servers/system/console/connection.c
@@ -1,7 +1,10 @@
 // Abstract: Connection handling in the land of Oz.
 // Author: Per Lundberg <per@halleluja.nu>
 //
-// © Copyright 2000, 2007, 2013 chaos development.
+// © Copyright 2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2013 chaos development
+// © Copyright 2015 chaos development
 
 #include <memory/memory.h>
 #include <video/video.h>
