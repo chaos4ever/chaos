223de52ea20df30438b6ae4d3bbd064ba88ca9e6
Per Lundberg <perlun@gmail.com>
2015-05-19 23:53:32 +0300

    Updated copyright year.

--- storm/include/storm/generic/system_call.h
+++ storm/include/storm/generic/system_call.h
@@ -1,7 +1,9 @@
 // Abstract: System call functions.
 // Author: Per Lundberg <per@halleluja.nu>
-
-// Copyright 2000, 2013 chaos development.
+//
+// © Copyright 2000 chaos development
+// © Copyright 2013 chaos development
+// © Copyright 2015 chaos development
 
 #pragma once
 
