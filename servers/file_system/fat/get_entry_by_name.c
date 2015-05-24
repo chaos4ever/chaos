e60ddf0f655bd23dc9e9934c8ec4aa922962d988
Per Lundberg <perlun@gmail.com>
2015-05-24 22:13:46 +0300

    Restructured the FAT server entirely.
    fat.c was incredibly long; around 1000 lines of code. I don't like that, so I decided to split it up into quite smallish files. More or less, one file per method (some methods are 200 lines long anyway, so it makes sense). To keep stuff easy to find, the names of the files match the method names in this case, except for fat.c which serves as entry point.
    
    I also fixed the usage of system_thread_create() which is now different since the signature has changed.

---
+++ servers/file_system/fat/get_entry_by_name.c
@@ -0,0 +1,129 @@
+// Abstract: Reads FAT entries matching a provided name.
+// Authors: Per Lundberg <per@halleluja.nu>
+//          Anders Öhrt <doa@chaosdev.org>
+//
+// © Copyright 1999-2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2015 chaos development
+
+#include <string/string.h>
+
+#include "fat.h"
+
+// Get a FAT entry matching the given name. Returns NULL if no matching entry is found.
+fat_entry_type *get_entry_by_name(fat_entry_type *fat_entry, const char *name)
+{
+    unsigned int entry;
+    bool done = FALSE;
+    unsigned int lfn_entries_in_row = 0;
+    char long_file_name[MAX_FILE_NAME_LENGTH];
+    bool has_lfn;
+
+    //  log_print_formatted (&log_structure, LOG_URGENCY_DEBUG, "name: %s", name);
+
+    // Now, parse the directory until we found our boy.
+    for (entry = 0; fat_entry[entry].name[0] != 0x00 && !done; entry++)
+    {
+        int counter;
+        char filename[13];
+
+        // First of all, make sure this isn't a long file entry.
+        if (fat_entry[entry].read_only == 1 &&
+            fat_entry[entry].hidden == 1 &&
+            fat_entry[entry].system == 1 &&
+            fat_entry[entry].volume_id == 1)
+        {
+            lfn_entries_in_row++;
+            continue;
+        }
+
+        // Also, make sure the file hasn't been deleted.
+        if (fat_entry[entry].name[0] == 0xE5)
+        {
+            continue;
+        }
+
+        // We don't want the volume ID either.
+        if (fat_entry[entry].volume_id == 1)
+        {
+            continue;
+        }
+
+        // Replace all spaces in the file name with zeroes, so that we get a NULL terminated string.
+        for (counter = 0; counter < 8; counter++)
+        {
+            if (fat_entry[entry].name[counter] == 0x20)
+            {
+                fat_entry[entry].name[counter] = 0;
+                break;
+            }
+        }
+
+        for (counter = 0; counter < 3; counter++)
+        {
+            if (fat_entry[entry].extension[counter] == 0x20)
+            {
+                fat_entry[entry].extension[counter] = 0;
+                break;
+            }
+        }
+
+        string_copy_max(filename, fat_entry[entry].name, 8);
+
+        // Make sure the string is zero terminated.
+        filename[8] = 0;
+
+        // If we have a file extension, add it too.
+        if (fat_entry[entry].extension[0] != 0)
+        {
+            unsigned int length = string_length(filename);
+            filename[string_length(filename)] = '.';
+            string_copy_max(&filename[string_length(filename)], fat_entry[entry].extension, 3);
+            filename[length + string_length(fat_entry[entry].extension)] = '\0';
+        }
+
+        // Get the long filename of the file (if any).
+        read_long_file_name(fat_entry, entry, lfn_entries_in_row, long_file_name);
+        lfn_entries_in_row = 0;
+
+        if (long_file_name[0] != '\0')
+        {
+            has_lfn = TRUE;
+        }
+        else
+        {
+            has_lfn = FALSE;
+        }
+
+        if (has_lfn)
+        {
+            if (string_compare(long_file_name, name) == 0)
+            {
+                //        log_print (&log_structure, LOG_URGENCY_DEBUG,
+                //                   "Matched a long file name");
+                done = TRUE;
+            }
+        }
+        else
+        {
+            if (string_compare(filename, name) == 0)
+            {
+                //        log_print (&log_structure, LOG_URGENCY_DEBUG,
+                //                   "Matched a short file name");
+                done = TRUE;
+            }
+        }
+    }
+
+    if (!done)
+    {
+        return NULL;
+    }
+
+    if (entry > 0)
+    {
+        entry--;
+    }
+
+    return &fat_entry[entry];
+}
