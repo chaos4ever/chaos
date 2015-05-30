ab61f5318caf852eaa502606fc24d49c2ae32b27
Per Lundberg <perlun@gmail.com>
2015-05-30 23:21:12 +0300

    How did via-rhine.c get into the picture...?

--- libraries/pci/functions.h
+++ libraries/pci/functions.h
@@ -9,17 +9,6 @@
 #include <ipc/ipc.h>
 #include <system/system.h>
 
-/*
-  functions from via-rhine.c. Which should be implemented, and how?
-
-  pci_dma_supported
-  pci_resource_len
-  pci_resource_start
-  pci_enable_device
-  pci_set_master
-  request_mem_region
-*/
-
 extern return_type pci_init(ipc_structure_type *pci_structure, tag_type *tag);
 extern return_type pci_get_number_of_devices(ipc_structure_type *pci_structure, unsigned int *number_of_devices);
 extern return_type pci_device_exists(ipc_structure_type *pci_structure, pci_device_probe_type *probe,
