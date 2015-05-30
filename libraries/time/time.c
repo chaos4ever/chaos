83e5d8d6cecbce72df3a6e0ff4aca147a86edf97
Per Lundberg <perlun@gmail.com>
2015-05-30 22:44:08 +0300

    Fixes for #2.

--- libraries/time/time.c
+++ libraries/time/time.c
@@ -1,70 +1,48 @@
-/* $Id$ */
-/* Abstract: Time/date library. */
-/* Author: Per Lundberg <per@halleluja.nu> */
-
-/* Copyright 2000 chaos development. */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
+// Abstract: Time/date library.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000 chaos development
+// © Copyright 2015 chaos development
 
 #include "time.h"
 
-/* Get the year number for the given chaos time. */
-
+// Get the year number for the given chaos time.
 unsigned int time_to_year(time_type time)
 {
     return time;
 }
 
-/* Get the month number for the given chaos time. */
-
+// Get the month number for the given chaos time.
 unsigned int time_to_month(time_type time)
 {
     return time;
 }
 
-/* Get the day of month number for the given chaos time. */
-
+// Get the day of month number for the given chaos time.
 unsigned int time_to_day(time_type time)
 {
     return time;
 }
 
-/* Get the hours count for the given chaos time. */
-
+// Get the hours count for the given chaos time.
 unsigned int time_to_hours(time_type time)
 {
     return ((time / 3600) % 24);
 }
 
-/* Get the minutes count for the given chaos time. */
-
+// Get the minutes count for the given chaos time.
 unsigned int time_to_minutes(time_type time)
 {
     return ((time / 60) % 60);
 }
 
-/* Get the seconds count for the given chaos time. */
-
+// Get the seconds count for the given chaos time.
 unsigned int time_to_seconds(time_type time)
 {
     return (time % 60);
 }
 
-/* Get the time. */
-
+// Get the time.
 time_type time_get(void)
 {
     kernelfs_time_type kernelfs_time;
--- libraries/time/time.h
+++ libraries/time/time.h
@@ -1,26 +1,10 @@
-/* $Id$ */
-/* Abstract: Time library header file. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: Time library header file.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000 chaos development
+// © Copyright 2015 chaos development
 
-/* Copyright 2000 chaos development. */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __LIBRARY_TIME_TIME_H__
-#define __LIBRARY_TIME_TIME_H__
+#pragma once
 
 #include <system/system.h>
 
@@ -33,5 +17,3 @@ extern unsigned int time_to_minutes(time_type time);
 extern unsigned int time_to_seconds(time_type time);
 
 extern time_type time_get(void);
-
-#endif /* !__LIBRARY_TIME_TIME_H__ */
