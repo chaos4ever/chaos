8b6ba3bcf127d0478de7c1b52e24f1e815c0a40e
Per Lundberg <perlun@gmail.com>
2015-08-21 20:27:27 +0300

    Fixes in line with #2.

--- servers/network/loopback/loopback.c
+++ servers/network/loopback/loopback.c
@@ -1,23 +1,21 @@
-/* Abstract: Loopback ethernet server for chaos. */
-/* Author: Per Lundberg <per@halleluja.nu> */
-
-/* Copyright 1999-2000 chaos development. */
-/* Copyright 2007 chaos development. */
-/* Copyright 2015 chaos development. */
+// Abstract: Loopback ethernet server for chaos.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 1999-2000 chaos development.
+// © Copyright 2007 chaos development.
+// © Copyright 2015 chaos development.
 
 #include "config.h"
 
 static log_structure_type log_structure;
 
-/* An empty tag list. */
-
+// An empty tag list.
 tag_type empty_tag =
 {
     0, 0, ""
 };
 
-/* Handle an IPC connection request. */
-
+// Handle an IPC connection request.
 static void handle_connection(mailbox_id_type *reply_mailbox_id)
 {
     system_call_thread_name_set("Handling connection");
@@ -31,8 +29,7 @@ static void handle_connection(mailbox_id_type *reply_mailbox_id)
 
     memory_allocate((void **) data_pointer, data_size);
 
-    /* Accept the connection. */
-
+    // Accept the connection.
     ipc_structure.output_mailbox_id = *reply_mailbox_id;
     ipc_connection_establish(&ipc_structure);
 
@@ -45,9 +42,7 @@ static void handle_connection(mailbox_id_type *reply_mailbox_id)
         message_parameter.message_class = IPC_CLASS_NONE;
         message_parameter.length = data_size;
 
-        if (ipc_receive(ipc_structure.input_mailbox_id, &message_parameter,
-                        &data_size) !=
-                STORM_RETURN_SUCCESS)
+        if (ipc_receive(ipc_structure.input_mailbox_id, &message_parameter, &data_size) != STORM_RETURN_SUCCESS)
         {
             continue;
         }
@@ -57,12 +52,9 @@ static void handle_connection(mailbox_id_type *reply_mailbox_id)
             case IPC_ETHERNET_REGISTER_TARGET:
             {
 #if FALSE
-                /* FIXME: Check if the protocol is already registered */
-
-                device->target[device->number_of_targets].mailbox_id =
-                    ipc_structure.output_mailbox_id;
-                device->target[device->number_of_targets].protocol_type =
-                    system_byte_swap_u16(data[0]);
+                // FIXME: Check if the protocol is already registered */
+                device->target[device->number_of_targets].mailbox_id = ipc_structure.output_mailbox_id;
+                device->target[device->number_of_targets].protocol_type = system_byte_swap_u16(data[0]);
                 device->number_of_targets++;
 #endif
                 break;
@@ -73,10 +65,9 @@ static void handle_connection(mailbox_id_type *reply_mailbox_id)
 #if FALSE
                 if (!el3_start_transmit(data, message_parameter.length, device))
                 {
-                    log_print(&log_structure, LOG_URGENCY_ERROR,
-                              "Failed to send an ethernet packet.");
+                    log_print(&log_structure, LOG_URGENCY_ERROR, "Failed to send an ethernet packet.");
 
-                    /* FIXME: Do something. */
+                    // FIXME: Do something.
                 }
 #endif
                 break;
@@ -87,8 +78,7 @@ static void handle_connection(mailbox_id_type *reply_mailbox_id)
 #if FALSE
                 memory_copy(data, &device->ethernet_address, 6);
                 message_parameter.length = 6;
-                system_call_mailbox_send(ipc_structure.output_mailbox_id,
-                                         &message_parameter);
+                system_call_mailbox_send(ipc_structure.output_mailbox_id, &message_parameter);
 #endif
                 break;
             }
@@ -103,31 +93,25 @@ static void handle_connection(mailbox_id_type *reply_mailbox_id)
     }
 }
 
-/* Main function. */
-
+// Main function.
 int main(void)
 {
     ipc_structure_type ipc_structure;
 
-    /* Set up our name. */
-
+    // Set up our name.
     system_call_process_name_set(PACKAGE_NAME);
     system_call_thread_name_set("Initialising");
 
     log_init(&log_structure, PACKAGE_NAME, &empty_tag);
 
-    /* Create the service. */
-
-    if (ipc_service_create("ethernet", &ipc_structure, &empty_tag) !=
-            IPC_RETURN_SUCCESS)
+    // Create the service.
+    if (ipc_service_create("ethernet", &ipc_structure, &empty_tag) != IPC_RETURN_SUCCESS)
     {
-        log_print(&log_structure, LOG_URGENCY_EMERGENCY,
-                  "Couldn't create ethernet service.");
+        log_print(&log_structure, LOG_URGENCY_EMERGENCY, "Couldn't create ethernet service.");
         return -1;
     }
 
-    /* Main loop. */
-
+    // Main loop.
     system_call_thread_name_set("Service handler");
 
     while (TRUE)
