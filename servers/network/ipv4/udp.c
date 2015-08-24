ffe635e788ef5b4a840174f18cc0e64cbb8326cc
Per Lundberg <perlun@gmail.com>
2015-08-24 07:27:59 +0300

    #2 fixes.

--- servers/network/ipv4/udp.c
+++ servers/network/ipv4/udp.c
@@ -1,9 +1,9 @@
-/* $Id$ */
-/* Abstract: UDP routines. */
-/* Author: Per Lundberg <per@halleluja.nu> */
-
-/* Copyright 1999-2000 chaos development */
-/* Copyright 2007 chaos development. */
+// Abstract: UDP routines.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 1999-2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2015 chaos development
 
 #include <log/log.h>
 #include <system/system.h>
@@ -14,24 +14,19 @@
 #include "socket.h"
 #include "udp.h"
 
-/* Locals. */
-
+// Locals.
 static socket_type **udp_hash = NULL;
 static unsigned int last_source_port = 1024;
 
-/* Initialise the UDP subsystem. */
-
+// Initialise the UDP subsystem.
 void udp_init(void)
 {
     socket_type ***udp_hash_pointer = &udp_hash;
-    memory_allocate((void **) udp_hash_pointer,
-                    SOCKET_HASH_SIZE * sizeof(socket_type *));
+    memory_allocate((void **) udp_hash_pointer, SOCKET_HASH_SIZE * sizeof(socket_type *));
 }
 
-/* Create a UDP header. */
-
-void udp_header_create(u16 destination_port, u16 source_port,
-                       u16 length, udp_header_type *udp_header)
+// Create a UDP header.
+void udp_header_create(u16 destination_port, u16 source_port, u16 length, udp_header_type *udp_header)
 {
     udp_header->destination_port = system_byte_swap_u16(destination_port);
     udp_header->source_port = system_byte_swap_u16(source_port);
@@ -41,8 +36,7 @@ void udp_header_create(u16 destination_port, u16 source_port,
     //                                     sizeof (udp_header_type));
 }
 
-/* Find the socket with the given socket ID. */
-
+// Find the socket with the given socket ID.
 static socket_type *udp_socket_find(unsigned int source_port)
 {
     socket_type *socket;
@@ -58,18 +52,15 @@ static socket_type *udp_socket_find(unsigned int source_port)
     return socket;
 }
 
-/* Insert this socket to the lists. */
-
+// Insert this socket to the lists.
 static void udp_socket_insert(socket_type *socket)
 {
     unsigned int hash_index = socket_get_hash(socket->source_port);
 
-    list_node_insert((list_type **) &udp_hash[hash_index],
-                     (list_type *) socket);
+    list_node_insert((list_type **) &udp_hash[hash_index], (list_type *) socket);
 }
 
-/* Find a free source UDP port. */
-
+// Find a free source UDP port.
 static unsigned int udp_find_port(void)
 {
     while (udp_socket_find(last_source_port) != NULL)
@@ -80,10 +71,8 @@ static unsigned int udp_find_port(void)
     return last_source_port;
 }
 
-/* Connect... */
-
-return_type udp_connect
-(u32 destination, unsigned int destination_port, unsigned int *socket_id)
+// Connect...
+return_type udp_connect(u32 destination, unsigned int destination_port, unsigned int *socket_id)
 {
     socket_type *socket;
     socket_type **socket_pointer = &socket;
@@ -95,20 +84,16 @@ return_type udp_connect
     socket->destination_address = destination;
     *socket_id = socket->socket_id = socket_get_free();
 
-    /* Add this socket to the hash tables. */
-
+    // Add this socket to the hash tables.
     udp_socket_insert(socket);
     socket_insert(socket);
 
-    /* FIXME: Add this socket to the list. */
-
+    // FIXME: Add this socket to the list.
     return UDP_RETURN_SUCCESS;
 }
 
-/* Send data on the given UDP socket. */
-
-return_type udp_send
-(void *data, unsigned int length, socket_type *socket)
+// Send data on the given UDP socket.
+return_type udp_send(void *data, unsigned int length, socket_type *socket)
 {
     u8 ethernet_address[IPV4_ETHERNET_ADDRESS_LENGTH];
     ipc_structure_type *ethernet_structure;
@@ -131,18 +116,14 @@ return_type udp_send
         return UDP_RETURN_INVALID_ARGUMENT;
     }
 
-    /* First, find out how we should route this packet. */
-
-    if (!route_find(socket->destination_address, &interface,
-                    &direct, &ethernet_structure))
+    // First, find out how we should route this packet.
+    if (!route_find(socket->destination_address, &interface, &direct, &ethernet_structure))
     {
         return UDP_RETURN_DESTINATION_UNREACHABLE;
     }
 
-    /* Get the ethernet address of the recipient. */
-
-    while (!arp_ip_to_ethernet_address(socket->destination_address,
-                                       ethernet_address))
+    // Get the ethernet address of the recipient.
+    while (!arp_ip_to_ethernet_address(socket->destination_address, ethernet_address))
     {
         log_print_formatted(&log_structure, LOG_URGENCY_DEBUG,
                             "Sending ARP who-has for %lX on %s.",
@@ -152,17 +133,14 @@ return_type udp_send
         system_sleep(500);
     }
 
-    /* Now, construct the package, and send it. */
-
+    // Now, construct the package, and send it.
     ipv4_ethernet_header_type **ethernet_header_pointer = &ethernet_header;
     memory_allocate((void **) ethernet_header_pointer,
                     sizeof(ipv4_ethernet_header_type) +
                     sizeof(ipv4_header_type) + sizeof(udp_header_type) +
                     length);
 
-    ipv4_ethernet_header_create
-    (ethernet_address, interface->hardware_address,
-     IPV4_ETHERNET_PROTOCOL_IPV4, ethernet_header);
+    ipv4_ethernet_header_create(ethernet_address, interface->hardware_address, IPV4_ETHERNET_PROTOCOL_IPV4, ethernet_header);
 
     ipv4_header = (ipv4_header_type *) &ethernet_header->data;
 
@@ -171,8 +149,7 @@ return_type udp_send
                        ipv4_header);
 
     udp_header = (udp_header_type *) &ipv4_header->data;
-    udp_header_create(socket->destination_port, socket->source_port,
-                      length, udp_header);
+    udp_header_create(socket->destination_port, socket->source_port, length, udp_header);
     memory_copy(&udp_header->data, data, length);
 
     message_parameter.protocol = IPC_PROTOCOL_ETHERNET;
@@ -187,34 +164,26 @@ return_type udp_send
     return UDP_RETURN_SUCCESS;
 }
 
-/* Receive an UDP packet and process it. */
-
-void udp_packet_receive
-(ipv4_interface_type *interface __attribute__((unused)),
- ipv4_ethernet_header_type *ethernet_header,
- int length, mailbox_id_type output_mailbox_id __attribute__((unused)))
+// Receive an UDP packet and process it.
+void udp_packet_receive(ipv4_interface_type *interface __attribute__((unused)), ipv4_ethernet_header_type *ethernet_header,
+                        int length, mailbox_id_type output_mailbox_id __attribute__((unused)))
 {
     ipv4_header_type *ipv4_header = (ipv4_header_type *) &ethernet_header->data;
-    udp_header_type *udp_header __attribute__((unused)) =
-        (udp_header_type *)((u8 *) ipv4_header + ipv4_header->header_length * 4);
+    udp_header_type *udp_header __attribute__((unused)) = (udp_header_type *)((u8 *) ipv4_header + ipv4_header->header_length * 4);
     char source_address[256], destination_address[256];
     socket_type *socket;
 
     ipv4_address_to_string(source_address, ipv4_header->source_address);
-    ipv4_address_to_string(destination_address,
-                           ipv4_header->destination_address);
+    ipv4_address_to_string(destination_address, ipv4_header->destination_address);
 
-    log_print_formatted
-    (&log_structure, LOG_URGENCY_DEBUG, "%s: UDP packet: %s:%u -> %s:%u.",
-     interface->identification, source_address,
-     system_big_endian_to_native_u16(udp_header->source_port),
-     destination_address,
-     system_big_endian_to_native_u16(udp_header->destination_port));
+    log_print_formatted(&log_structure, LOG_URGENCY_DEBUG, "%s: UDP packet: %s:%u -> %s:%u.",
+                        interface->identification, source_address,
+                        system_big_endian_to_native_u16(udp_header->source_port),
+                        destination_address,
+                        system_big_endian_to_native_u16(udp_header->destination_port));
 
-    /* Check if this packet should be delivered somewhere. */
-
-    socket = udp_socket_find
-             (system_big_endian_to_native_u16(udp_header->destination_port));
+    // Check if this packet should be delivered somewhere.
+    socket = udp_socket_find(system_big_endian_to_native_u16(udp_header->destination_port));
 
     if (socket != NULL)
     {
@@ -224,19 +193,15 @@ void udp_packet_receive
         log_print(&log_structure, LOG_URGENCY_DEBUG, "Buffering packet");
 
         memory_allocate((void **) packet_pointer, sizeof(packet_list_type));
-        packet->length = (length - sizeof(ipv4_ethernet_header_type) -
-                          sizeof(ipv4_header_type) - sizeof(udp_header_type));
+        packet->length = (length - sizeof(ipv4_ethernet_header_type) - sizeof(ipv4_header_type) - sizeof(udp_header_type));
         packet->source_address = ipv4_header->source_address;
-        packet->source_port =
-            system_big_endian_to_native_u16(udp_header->source_port);
+        packet->source_port = system_big_endian_to_native_u16(udp_header->source_port);
         memory_allocate((void **) &packet->data, packet->length);
         memory_copy(packet->data, udp_header->data, packet->length);
 
-        log_print_formatted(&log_structure, LOG_URGENCY_DEBUG, "ull: %u",
-                            packet->length);
+        log_print_formatted(&log_structure, LOG_URGENCY_DEBUG, "ull: %u", packet->length);
 
         packet_list_type **packet_list_pointer = &socket->packet_list;
-        list_node_insert((list_type **) packet_list_pointer,
-                         (list_type *) packet);
+        list_node_insert((list_type **) packet_list_pointer, (list_type *) packet);
     }
 }
--- servers/network/ipv4/udp.h
+++ servers/network/ipv4/udp.h
@@ -1,36 +1,18 @@
-/* $Id$ */
-/* Abstract: UDP prototypes and structure definitions. */
-/* Author: Per Lundberg <per@halleluja.nu>. */
+// Abstract: UDP prototypes and structure definitions.
+// Author: Per Lundberg <per@halleluja.nu>.
+//
+// © Copyright 1999-2000 chaos development
+// © Copyright 2015 chaos development
 
-/* Copyright 1999-2000 chaos development. */
+#pragma once
 
-/* This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __UDP_H__
-#define __UDP_H__
-
-/* Read more about UDP in RFC 768. */
+// Read more about UDP in RFC 768.
 
 #include <system/system.h>
 #include "socket.h"
 
-/* Defines/enums. */
-
-/* UDP ports. */
-
+// Defines/enums.
+// UDP ports.
 enum
 {
     UDP_DHCP_REQUEST = 67,
@@ -46,42 +28,25 @@ enum
     UDP_RETURN_DESTINATION_UNREACHABLE,
 };
 
-/* Type definitions. */
-
-/* An UDP header. */
-
+// Type definitions.
+// An UDP header.
 typedef struct
 {
     u16 source_port;
     u16 destination_port;
     u16 length;
 
-    /* The checksum is for all of the packet. */
-
+    // The checksum is for all of the packet.
     u16 check;
 
-    /* Data starts here. */
-
+    // Data starts here.
     u8 data[0];
 } __attribute__((packed)) udp_header_type;
 
-/* Function prototypes. */
-
-extern void udp_header_create
-(u16 destination_port, u16 source_port,
- u16 length, udp_header_type *udp_header);
-
-extern void udp_packet_receive
-(ipv4_interface_type *interface,
- ipv4_ethernet_header_type *ethernet_header,
- int length __attribute__((unused)), mailbox_id_type output_mailbox_id);
-
-return_type udp_connect
-(u32 destination, unsigned int destination_port, unsigned int *socket_id);
-
+// Function prototypes.
+extern void udp_header_create(u16 destination_port, u16 source_port, u16 length, udp_header_type *udp_header);
+extern void udp_packet_receive(ipv4_interface_type *interface, ipv4_ethernet_header_type *ethernet_header,
+                               int length __attribute__((unused)), mailbox_id_type output_mailbox_id);
+return_type udp_connect(u32 destination, unsigned int destination_port, unsigned int *socket_id);
 extern void udp_init(void);
-
-extern return_type udp_send
-(void *data, unsigned int length, socket_type *socket);
-
-#endif /* !__UDP_H__ */
+extern return_type udp_send(void *data, unsigned int length, socket_type *socket);
