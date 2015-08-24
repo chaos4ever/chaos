e7ba9e89c8a59a90c578add0d670790a916dc44c
Per Lundberg <perlun@gmail.com>
2015-08-24 07:14:25 +0300

    #2 fixes.

--- servers/network/ipv4/socket.c
+++ servers/network/ipv4/socket.c
@@ -1,9 +1,9 @@
-/* $Id$ */
-/* Abstract: Socket management. */
-/* Author: Per Lundberg <per@halleluja.nu> */
-
-/* Copyright 2000 chaos development. */
-/* Copyright 2007 chaos development. */
+// Abstract: Socket management.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2015 chaos development
 
 #include "config.h"
 #include "ipv4.h"
@@ -13,15 +13,13 @@
 static ipv4_socket_id_type last_socket_id = 0;
 static socket_type **socket_hash = NULL;
 
-/* Get the hash for this socket ID. */
-
+// Get the hash for this socket ID.
 ipv4_socket_id_type socket_get_hash(ipv4_socket_id_type socket_id)
 {
     return socket_id % SOCKET_HASH_SIZE;
 }
 
-/* Get the first free socket ID type. */
-
+// Get the first free socket ID type.
 ipv4_socket_id_type socket_get_free(void)
 {
     while (socket_find(last_socket_id) != NULL)
@@ -32,37 +30,30 @@ ipv4_socket_id_type socket_get_free(void)
     return last_socket_id;
 }
 
-/* Initialise the socket support. */
-
+// Initialise the socket support.
 void socket_init(void)
 {
     socket_type ***socket_hash_pointer = &socket_hash;
-    memory_allocate((void **) socket_hash_pointer,
-                    SOCKET_HASH_SIZE * sizeof(socket_type *));
+    memory_allocate((void **) socket_hash_pointer, SOCKET_HASH_SIZE * sizeof(socket_type *));
 }
 
-/* Insert the given socket into the hash table. */
-
+// Insert the given socket into the hash table.
 void socket_insert(socket_type *socket)
 {
     ipv4_socket_id_type socket_id = socket_get_hash(socket->socket_id);
 
-    list_node_insert((list_type **) &socket_hash[socket_id],
-                     (list_type *) socket);
+    list_node_insert((list_type **) &socket_hash[socket_id], (list_type *) socket);
 }
 
-/* Delete the given socket. */
-
+// Delete the given socket.
 void socket_delete(socket_type *socket)
 {
     ipv4_socket_id_type socket_id = socket_get_hash(socket->socket_id);
 
-    list_node_delete((list_type **) &socket_hash[socket_id],
-                     (list_type *) socket);
+    list_node_delete((list_type **) &socket_hash[socket_id], (list_type *) socket);
 }
 
-/* Find the socket with the given socket ID. */
-
+// Find the socket with the given socket ID.
 socket_type *socket_find(ipv4_socket_id_type socket_id)
 {
     socket_type *socket;
@@ -78,17 +69,14 @@ socket_type *socket_find(ipv4_socket_id_type socket_id)
     return socket;
 }
 
-/* Send data on the given socket. */
-
-return_type socket_send
-(ipv4_socket_id_type socket_id, unsigned int length, void *data)
+// Send data on the given socket.
+return_type socket_send(ipv4_socket_id_type socket_id, unsigned int length, void *data)
 {
     socket_type *socket = socket_find(socket_id);
 
     if (socket == NULL)
     {
-        log_print_formatted(&log_structure, LOG_URGENCY_DEBUG,
-                            "Socket %u not found!", socket_id);
+        log_print_formatted(&log_structure, LOG_URGENCY_DEBUG, "Socket %u not found!", socket_id);
         return SOCKET_RETURN_INVALID_ARGUMENT;
     }
 
@@ -100,10 +88,8 @@ return_type socket_send
     return SOCKET_RETURN_SUCCESS;
 }
 
-/* Read data from the given socket. */
-
-void socket_receive
-(ipv4_socket_id_type socket_id, mailbox_id_type mailbox_id)
+// Read data from the given socket.
+void socket_receive(ipv4_socket_id_type socket_id, mailbox_id_type mailbox_id)
 {
     socket_type *socket = socket_find(socket_id);
     message_parameter_type message_parameter;
@@ -111,21 +97,16 @@ void socket_receive
 
     if (socket == NULL)
     {
-        log_print_formatted(&log_structure, LOG_URGENCY_DEBUG,
-                            "Socket %u not found!", socket_id);
+        log_print_formatted(&log_structure, LOG_URGENCY_DEBUG, "Socket %u not found!", socket_id);
 
-        /* FIXME: If this occurs, the receiver will sleep forever. */
-
+        // FIXME: If this occurs, the receiver will sleep forever.
         return;
     }
 
-    /* Wait until there is data available. */
-
+    // Wait until there is data available.
     while (socket->packet_list == NULL)
     {
-        /* FIXME: We should have some kind of semaphore system (with real
-           blocking) for this. */
-
+        // FIXME: We should have some kind of semaphore system (with real blocking) for this.
         system_sleep(100);
     }
 
@@ -142,10 +123,8 @@ void socket_receive
     message_parameter.length = sizeof(ipv4_receive_type);
     ipc_send(mailbox_id, &message_parameter);
 
-    log_print_formatted(&log_structure, LOG_URGENCY_DEBUG,
-                        "ULL: %u", socket->packet_list->length);
+    log_print_formatted(&log_structure, LOG_URGENCY_DEBUG, "ULL: %u", socket->packet_list->length);
 
     packet_list_type **packet_list_pointer = &socket->packet_list;
-    list_node_delete((list_type **) packet_list_pointer,
-                     (list_type *) socket->packet_list);
+    list_node_delete((list_type **) packet_list_pointer, (list_type *) socket->packet_list);
 }
--- servers/network/ipv4/socket.h
+++ servers/network/ipv4/socket.h
@@ -1,31 +1,14 @@
-/* $Id$ */
-/* Abstract: Socket handling. */
-/* Author: Per Lundberg <per@halleluja.nu> */
+// Abstract: Socket handling.
+// Author: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2000 chaos development
+// © Copyright 2015 chaos development
 
-/* Copyright 2000 chaos development. */
-
-/* This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License as
-   published by the Free Software Foundation; either version 2 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
-
-#ifndef __SOCKET_H__
-#define __SOCKET_H__
+#pragma once
 
 #include "packet.h"
 
-/* Defines/enums. */
-
+// Defines/enums.
 #define SOCKET_HASH_SIZE        1024
 
 enum
@@ -34,8 +17,7 @@ enum
     SOCKET_RETURN_INVALID_ARGUMENT
 };
 
-/* Type definitions. */
-
+// Type definitions.
 typedef struct
 {
     struct socket_type *next;
@@ -45,30 +27,21 @@ typedef struct
     unsigned int source_port;
     unsigned int destination_port;
 
-    /* FIXME: We should have a source address too, so that different
-       applications can listen to different interfaces. */
-
+    // FIXME: We should have a source address too, so that different applications can listen to different interfaces.
     ipv4_address_type destination_address;
+
     unsigned int socket_id;
 
-    /* A list of packet waiting on this socket. */
-
+    // A list of packet waiting on this socket.
     packet_list_type *packet_list;
 } socket_type;
 
-/* Function prototypes. */
-
+// Function prototypes.
 extern void socket_init(void);
 extern ipv4_socket_id_type socket_get_free(void);
 extern socket_type *socket_find(ipv4_socket_id_type socket_id);
 extern void socket_insert(socket_type *socket);
 extern void socket_delete(socket_type *socket);
 extern ipv4_socket_id_type socket_get_hash(ipv4_socket_id_type socket_id);
-
-extern return_type socket_send
-(ipv4_socket_id_type socket_id, unsigned int length, void *data);
-
-extern void socket_receive
-(ipv4_socket_id_type socket_id, mailbox_id_type mailbox_id);
-
-#endif /* !__SOCKET_H__ */
+extern return_type socket_send(ipv4_socket_id_type socket_id, unsigned int length, void *data);
+extern void socket_receive(ipv4_socket_id_type socket_id, mailbox_id_type mailbox_id);
