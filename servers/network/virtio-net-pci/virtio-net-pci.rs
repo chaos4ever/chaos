8246724e237f4c3eefb222710ac53a33a89d817e
Per Lundberg <perlun@gmail.com>
2015-11-22 21:17:55 +0200

    Started fleshing out the skeleton for calling the 'log' and 'pci' services.

--- servers/network/virtio-net-pci/virtio-net-pci.rs
+++ servers/network/virtio-net-pci/virtio-net-pci.rs
@@ -1,3 +1,8 @@
+// Abstract: Entry point for the virtio-net-pci network server.
+// Authors: Per Lundberg <per@halleluja.nu>
+//
+// © Copyright 2015 chaos development.
+
 #![feature(core_slice_ext)]
 #![feature(core_str_ext)]
 #![feature(lang_items)]
@@ -6,6 +11,8 @@
 
 #![no_std]
 
+mod log;
+mod pci;
 mod system;
 
 #[no_mangle]
@@ -13,5 +20,13 @@ pub unsafe extern fn main() -> i32 {
     system::process_name_set("virtio-net-pci");
     system::thread_name_set("Initialising");
 
-    loop {}
+    let log_client = log::Client.new("virtio-net-pci").init();
+
+    let pci_client = pci::Client.new();
+    if (pci_client.init() != pci::ReturnValues::Success) {
+        log_client.print(log::Urgencies::Emergency, "Couldn't create connection to PCI service.");
+        return -1;
+    }
+
+    0
 }
