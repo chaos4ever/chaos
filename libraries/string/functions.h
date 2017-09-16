4a15db4340792c08b3c85f7dbc958484c76a2b96
Per Lundberg <perlun@gmail.com>
2017-09-16 22:27:18 +0300

    libraries/string: Fixes for #2

--- libraries/string/functions.h
+++ libraries/string/functions.h
@@ -1,22 +1,19 @@
-/* $Id$ */
-/* Abstract: Function prototypes. */
-/* Author: Per Lundberg <per@chaosdev.io> */
+// Abstract: Function prototypes.
+// Author: Per Lundberg <per@chaosdev.io>
 
-/* Copyright 2000 chaos development. */
-/* Copyright 2007 chaos development. */
+// © Copyright 2000 chaos development
+// © Copyright 2007 chaos development
+// © Copyright 2017 chaos development
 
-#ifndef __LIBRARY_STRING_FUNCTIONS_H__
-#define __LIBRARY_STRING_FUNCTIONS_H__
+#pragma once
 
 #include <stdarg.h>
 #include <system/system.h>
 
-extern return_type string_to_number (const char *string, int *number,
-                                     unsigned int *characters);
+extern return_type string_to_number(const char *string, int *number,
+                                    unsigned int *characters);
 
-extern return_type string_print (char *output, const char *format_string, ...)
-  __attribute__ ((format (printf, 2, 3)));
-extern return_type string_print_va (char *output, const char *format_string,
-                                    va_list arguments);
-
-#endif /* !defined __LIBRARY_STRING_FUNCTIONS_H__ */
+extern return_type string_print(char *output, const char *format_string, ...)
+__attribute__((format(printf, 2, 3)));
+extern return_type string_print_va(char *output, const char *format_string,
+                                   va_list arguments);
