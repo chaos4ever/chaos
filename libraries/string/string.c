28ff222383d7b687e770eccc9a6c8e13f8ebefe6
Per Lundberg <per.lundberg@ecraft.com>
2017-09-27 00:19:33 +0300

    TIL: Explicit case fallthrough
    More details: https://developers.redhat.com/blog/2017/03/10/wimplicit-fallthrough-in-gcc-7/

--- libraries/string/string.c
+++ libraries/string/string.c
@@ -538,6 +538,10 @@ return_type string_print_va(char *output, const char *format_string,
                     case 'X':
                     {
                         flags |= UPPER_HEX;
+
+                        // Falls through by design. (Can't use __attribute__ ((fallthrough)), since it breaks on gcc < 7.)
+                        //
+                        /* @fallthrough@ */
                     }
 
                     case 'x':
