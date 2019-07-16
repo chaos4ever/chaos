5241579369438fb8f96e8595acd96fc783da1b85
Per Lundberg <perlun@gmail.com>
2019-07-16 21:45:07 +0300

    [servers/keyboard] Minor #2 fix

--- servers/system/keyboard/init.c
+++ servers/system/keyboard/init.c
@@ -1,10 +1,7 @@
 // Abstract: Keyboard initialisation code.
 // Author: Per Lundberg <per@chaosdev.io>
 //
-// © Copyright 2000 chaos development
-// © Copyright 2007 chaos development
-// © Copyright 2013 chaos development
-// © Copyright 2015-2016 chaos development
+// © Copyright 1999 chaos development
 
 #include <memory/memory.h>
 #include <system/system.h>
