de12259c1230a655e254e1aa7aeeeb63c4007205
Per Lundberg <perlun@gmail.com>
2019-08-25 22:06:22 +0300

    (pci) Include vendor and device ID in pci_device_info_type (#162)
    The use case is this: For device drivers which supports multiple PCI
    device ID:s, it is sometimes important to be able to differentiate minor
    details in the hardware-related code depending on device ID. For
    example, the EEPROM of a network card might be read using slightly
    different mechanisms on different device ID:s, whereas the rest of the
    hardware support (PHY etc) is entirely identical.
    
    So, to be able to better support such use cases, we are now adding a few
    fields to the pci_device_info_type, which is used when probing for PCI
    devices.

--- libraries/ipc/pci.h
+++ libraries/ipc/pci.h
@@ -1,8 +1,7 @@
 // Abstract: Protocol for communicating with the PCI server.
 // Author: Per Lundberg <per@chaosdev.io>
 //
-// © Copyright 1999-2000 chaos development
-// © Copyright 2013-2017 chaos development
+// © Copyright 1999 chaos development
 
 #pragma once
 
@@ -84,5 +83,7 @@ typedef struct
 typedef struct
 {
     unsigned int irq;
+    uint16_t vendor_id;
+    uint16_t device_id;
     pci_resource_type resource[PCI_NUMBER_OF_RESOURCES];
 } pci_device_info_type;
